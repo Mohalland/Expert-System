480650d2e181e3046577c0750d6e74b0e306a4b2
Mohalland <lmohalland@gmail.com>
2015-03-23 23:53:21 -0500

    Update main.pl

--- main.pl
+++ main.pl
@@ -1,9 +1,43 @@
 % Author:   L.Mohalland, D.Smith, M.McMre, Rosue
 % Date: 3/22/2015
 
+%Common Criminal Charges
+
+%offence(code, title)
+offence(aNaRa, title('Aiding & Abetting / Accessory')).
+offence(aRb, title('Assault / Battery')).
+offence(drug-possession, title('Drug Possession')).
+offence(burglary, title('Burglary')).
+offence(aRL, title('Theft / Larceny')).
+
+
+%All Other Charges
+offence(child-Abuse, title('Child Abuse')).
+offence(computer-crime, title('Computer Crime')).
+offence(domestic-violence, title('Domestic Violence')).
+offence(disorderly-conduct, title('Disorderly Conduct')).
+offence(extortion, title('Extortion')).
+offence(embezzlement, title('Embezzlement')).
+offence(forgery, title('Forgery')).
+offence(fraud, title('Fraud')).
+offence(harassment, title('Harassment')).
+offence(homicide, title('Homicide')).
+offence(indecent-exposure, title('Indecent Exposure')).
+offence(identity-theft, title('Identity Theft')).
+offence(kidnapping, title('Kidnapping')).
+offence(manslaughter-involuntary, title('Manslaughter: Involuntary')).
+offence(manslaughter-voluntary, title('Manslaughter: Voluntary')).
+offence(prostitution, title('Prostitution')).
+offence(public-intoxication, title('Public Intoxication')).
+offence(rape, title('Rape')).
+offence(robbery, title('Robbery')).
+offence(sexual-assault, title('Sexual Assault')).
+offence(shoplifting, title('Shoplifting')).
+
 %gender (male, female)
 gender(male).
 gender(female).
+
 %eye color (amber, black, blue, brown, gray, green, hazel, violet)
 eye_colour(amber).
 eye_colour(black).
@@ -12,19 +46,23 @@ eye_colour(brown).
 eye_colour(gray).
 eye_colour(hazel).
 eye_colour(violet).
+
 %skin color  (dark, light)
 skin_colour(dark).
 skin_colour(light).
+
 %height   (ft and inches)
 %height(F,Inches).
 %weight (pounds)
 %special features( tattoo/scars)
 special_features(scars).
+
 %hair color  (black, brown, red, blonde, )
 hair_colour(black).
 hair_colour(brown).
 hair_colour(red).
 hair_colour(blonde).
+
 %hair style(afro, afro textured, etc...)
 hair_style(afro, 'Big hair hairstyle, featured heavily in Afro-American culture, popular through the 1970s in the United States of America').
 hair_style(afro-textured, 'A typical texture of Black African hair that has not been altered by hot combs, flat irons, or chemicals (by perming, relaxing, or straightening). Each strand of this hair type grows in a tiny spring-like, corkscrew shape. The overall effect is such that, despite relatively fewer actual hair shafts compared to straight hair,[1] this texture appears (and feels) denser than its straight counterparts. Due to this, it is often referred to as thick, bushy, or woolly.').
@@ -46,14 +84,15 @@ hair_style(pony-tail, 'Hair that is pulled to the back of the head and often hel
 hair_style(quiff, 'The quiff combines the 1950s pompadour hairstyle, the 1950s flat-top, and, sometimes a mohawk').
 hair_style(wave,'Short Hair Waves, shortened to just waves, is a very common and sought after hairstyle for African American men that create the appearance of water like waves of the hair').
 
-%human(name, gender, eye colour, hair colour, special features, hair style, skin colur, height, weight)
+
 %populate suspects/known offenders
+%human(name, gender, eye colour, hair colour, special features, hair style, skin colur, height, weight)
 suspect(fname(nestor), lname(schnabel), gender(male), eye_colour(blue), hair_colour(brown), special_features(['dragon tattoo shoulder', 'scar right eye']), hair_style(wave), skin_colour(dark), height([5,9])).
 suspect(fname(freeman), lname(haverty), gender(male), eye_colour(brown), hair_colour(black), special_features('dragon tattoo chest'), hair_style(mohawk), skin_colour(light), height([5,11])).
 suspect(fname(darcie), lname(merino), gender(female), eye_colour(blue), hair_colour(blonde), special_features ).
 suspect(fname(oralia), lname(keplin), gender(female), eye_colour(hazel), hair_colour(brown), special_features ).
 suspect(fname(erline), lname(vierra), gender(female), eye_colour(hazel), hair_colur(blande), special_features(none),  ).
-suspect(fname(mistie), lanme(Rayborn), gender(female),  ).
+suspect(fname(mistie), lanme(Rayborn), gender(female),  eye_colour(gray) ).
 suspecct(fname(roslyn) lname(rickman), gender(female),  ).
 suspect(fname(hilton) lname(laflamme), gender(male)    ).
 suspect(Roland Buckner gender(male)
@@ -67,6 +106,7 @@ suspect(fname(Lynetta Reisman gender(female)
 suspect(fname(Devin Wix gender(female)
 suspect(female(Vernon Ory gender(female)
 suspect(fname(Callie Grever gender(male)
+
 expert_system:-
                nl,write('Enter suspect Race: '), read(Race),
                nl,write('Enter suspect skin colour: '), read(Skin_colour),
